0268c70d5d51efb3da73ad0d0126917e3be946ff
Dropth <florian.alline@etu.univ-lehavre.fr>
2016-10-10 10:30:54 +0200

    test is ok

--- src/ihm/IHM_Fichier.java
+++ src/ihm/IHM_Fichier.java
@@ -4,7 +4,8 @@ package ihm;
  * Created by thiba on 10/10/2016.
  */
 public class IHM_Fichier {
-    public static void test1(){
-        System.out.println("isOk");
-    }
+
+        public static void test2 () {
+            System.out.println("Coucou");
+        }
 }
